93c02fe9d17ff29947dd050a68b9f1283d71c4c9
Nguyen Tran <merlinvn@gmail.com>
2021-02-24 17:16:45 -0500

    update useful tools for linux

---
+++ wiki/os/cheat.sh
@@ -0,0 +1,16 @@
+= Install =
+
+To install the client:
+{{{bash
+mkdir -p ~/bin/
+curl https://cht.sh/:cht.sh > ~/bin/cht.sh
+chmod +x ~/bin/cht.sh
+}}}
+or to install it globally (for all users):
+{{{bash
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
+chmod +x /usr/local/bin/cht.sh
+}}}
+
+
+Ref: [[https://github.com/chubin/cheat.sh#command-line-client-chtsh|Github Page]]
